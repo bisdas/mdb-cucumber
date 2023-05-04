e90f7f003908e2dd8e3e9f004c9fc3409ee9b706
Bishnu <16491515+bisdas@users.noreply.github.com>
2023-05-05 01:40:13 +0530

    chore: updated products catalog

--- src/storeCatalog/catalogs.ts
+++ src/storeCatalog/catalogs.ts
@@ -318,6 +318,30 @@ export const BRANDS = {
         id: 'Caffeine',
         title: 'Caffeine',
     },
+    InstaCuppa: {
+        id: 'InstaCuppa',
+        title: 'InstaCuppa',
+    },
+    Tesora: {
+        id: 'Tesora',
+        title: 'Tesora',
+    },
+    Mystic_Leaf: {
+        id: 'Mystic_Leaf',
+        title: 'Mystic Leaf',
+    },
+    Havells: {
+        id: 'Havells',
+        title: 'Havells',
+    },
+    KENT: {
+        id: 'KENT',
+        title: 'KENT',
+    },
+    Inalsa: {
+        id: 'Inalsa',
+        title: 'Inalsa',
+    },
 }
 
 export const TAGS = {
