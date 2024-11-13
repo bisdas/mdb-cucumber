f00293cd3a4b12b70ffefa106b38521e9c3e96dd
bisdas <16491515+bisdas@users.noreply.github.com>
2024-11-13 22:57:13 +0530

    chore: added hyperlink button

---
+++ src/components/flexible/HyperlinkButton/HyperlinkButton.styled.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components/macro'
+
+export type HyperlinkButtonAppearance = 'contained' | 'outlined' | 'textonly'
+
+const backgroundColorDefault = 'unset'
+const backgroundColorContained = '#0b9d58'
+const outlineColor = '#0b9d58'
+const textColorDefault = '#000000'
+const textColorContained = '#ffffff'
+
+interface IOuterWrapperProps {
+    appearance: HyperlinkButtonAppearance
+}
+export const OuterWrapper = styled.a<IOuterWrapperProps>`
+    font-family: Montserrat-Bold, 'Helvetica Neue', Helvetica, 'sans-serif';
+    font-size: 14px;
+    padding: 10px 16px;
+    cursor: pointer;
+    display: inline-block;
+    user-select: none;
+    outline: none;
+    background: ${(props) => (props.appearance === 'contained' ? backgroundColorContained : backgroundColorDefault)};
+    border-style: ${(props) => (props.appearance === 'outlined' ? `solid` : 'none')};
+    border-width: 2px;
+    border-color: ${outlineColor};
+    border-radius: 3px;
+    box-sizing: border-box;
+    text-decoration: none;
+`
+
+interface ITextProps {
+    appearance: HyperlinkButtonAppearance
+}
+
+export const Text = styled.div<ITextProps>`
+    color: ${(props) => (props.appearance === 'contained' ? textColorContained : textColorDefault)};
+`
---
+++ src/components/flexible/HyperlinkButton/index.tsx
@@ -0,0 +1,18 @@
+import React, { type FunctionComponent, type ReactElement } from 'react'
+import { OuterWrapper, Text, type HyperlinkButtonAppearance } from './HyperlinkButton.styled'
+
+interface IHyperlinkButtonProps {
+    appearance: HyperlinkButtonAppearance
+    children: string
+    href: string
+}
+
+const HyperlinkButton: FunctionComponent<IHyperlinkButtonProps> = ({ children, appearance, href }): ReactElement => {
+    return (
+        <OuterWrapper appearance={appearance} role="button" href={href} target="_blank">
+            <Text appearance={appearance}>{children}</Text>
+        </OuterWrapper>
+    )
+}
+
+export default HyperlinkButton
